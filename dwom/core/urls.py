50cf3e403d923ed0cda4c6c5431e85de6309be3c
Gideon Tettehfio <gideonlambride@gmail.com>
2024-09-10 23:27:41 +0000

    updates

--- dwom/core/urls.py
+++ dwom/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from item import views as item_views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+    path('items/<int:id>', item_views.detail, name='detail'),
 ]
---
+++ dwom/item/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Item
+# Create your views here.
+
+
+def detail(request, id):
+    item = get_object_or_404(Item, id=id)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(id=id)[0:3]
+    context = {'item': item, 'related_items': related_items}
+    return render(request, 'item/detail.html', context)
